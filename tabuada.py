3d3a7606caf176f23f94268fb8967cfe36f0a7b0
Gregorio Honorato <greghono@gmail.com>
2020-02-10 13:20:17 -0300

    Revert "ACRESCENTANDO A TABUADA DO 10"
    This reverts commit 09d967b437a90ddd17d52b78919ca6d7294524a0.

--- tabuada.py
+++ tabuada.py
@@ -1,6 +1,6 @@
 with open('tabuada.txt', 'w') as arq:
-    for i in range(11):
-        for ii in range(11):
+    for i in range(10):
+        for ii in range(10):
             arq.write(f"{i} * {ii} = {ii*i}\n")
         arq.write("-"*20 + '\n')
         
